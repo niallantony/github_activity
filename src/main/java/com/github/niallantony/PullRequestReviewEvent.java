14143dc28d84eb2d8d8b266c00848e61126e0b4d
Niall Antony <88824327+niallantony@users.noreply.github.com>
2025-04-05 19:37:16 +0900

    Create PullRequestReviewEvent class

---
+++ src/main/java/com/github/niallantony/PullRequestReviewEvent.java
@@ -0,0 +1,31 @@
+package com.github.niallantony;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+import lombok.Getter;
+
+public class PullRequestReviewEvent extends GitEvent {
+
+  private @Getter String title;
+
+  public PullRequestReviewEvent(JsonNode event) {
+    super(event);
+    this.title = this.payload.get("pull_request").get("title").asText();
+  }
+
+  @Override
+  public String toString() {
+    return String.format("Reviewed a pull request in %s: %s", this.repo_name, this.title);
+  }
+
+  @Override
+  public boolean isSimilar(GitEvent other) {
+    if (other.getClass() != PullRequestEvent.class)
+      return false;
+    PullRequestEvent next = (PullRequestEvent) other;
+    if (this.repo_name.equals(other.getRepo())
+        && next.getTitle().equals(this.title))
+      return true;
+    return false;
+  }
+}
