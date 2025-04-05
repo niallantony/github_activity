9c43098c9252dc27aced632487e59dd3cb66a510
Niall Antony <88824327+niallantony@users.noreply.github.com>
2025-04-05 22:46:24 +0900

    Remove forgotten System.out line

--- src/main/java/com/github/niallantony/SponsorshipEvent.java
+++ src/main/java/com/github/niallantony/SponsorshipEvent.java
@@ -3,9 +3,12 @@ package com.github.niallantony;
 import com.fasterxml.jackson.databind.JsonNode;
 
 public class SponsorshipEvent extends GitEvent {
+  /**
+   * TO-DO: Understand what a sponsorship event pertains to...
+   * Receiving a sponsorship, sponsoring another, then what is the repo?
+   */
   public SponsorshipEvent(JsonNode event) {
     super(event);
-    System.out.println(event.toPrettyString());
   }
 
   @Override
